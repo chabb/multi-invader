f0aa5d2a220aac4ab4fada83d828c49f581a607e
U120655 <U120655@mobi.ch>
2023-10-18 11:22:52 +0200

    scaffold color management

---
+++ src/players/bulet-color.ts
@@ -0,0 +1,4 @@
+export const colors = ['red', 'blue', 'amber', '#3A5311', '#601A35', 'yellow', 'green', '#FE7D6A'];
+let counter = 0;
+export const getColors = () => colors[counter++ % colors.length];
+//TODO(write test)
